448c6ac13692741436c6352843db9f26a16665bf
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:22:33 +0500

    Add files via upload
    A dinner guest list was made.

---
+++ Q19.js
@@ -0,0 +1,4 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let person_name = [" Jennifer", "Aster", "Anum", "Noman"];
+console.log(`I am invited ${person_name.length} people to Dinner.`);
